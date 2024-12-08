ea87365ef0fd77d2d1f4386ddc05912c72a47735
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-12-09 00:03:07 +0100

    feat: fix open api and add wake and shutdown apis

--- wol-api/src/main.rs
+++ wol-api/src/main.rs
@@ -1,19 +1,19 @@
-use core::str::FromStr;
+use core::convert::Infallible;
 use figment::{
     providers::{Format as _, Yaml},
     Figment,
 };
 use machine::Store;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::path::PathBuf;
 use utoipa::{OpenApi, ToSchema};
 use utoipa_rapidoc::RapiDoc;
-use std::{collections::HashMap, convert::Infallible, path::PathBuf, sync::Mutex};
-use warp::Filter;
-use wol::MacAddr;
+use warp::{reply, Filter};
 
 use anyhow::Context as _;
 use clap::Parser;
-use log::{debug, info};
+use log::debug;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -28,24 +28,30 @@ struct Args {
     config_path: PathBuf,
 }
 
-#[derive(Serialize, Deserialize, Clone, ToSchema)]
+#[derive(Serialize, Deserialize, Clone, ToSchema, Debug)]
 struct MachineConfig {
     #[schema(example = "192.168.1.4")]
-    ip: String,
+    pub ip: String,
     #[schema(example = "f4:93:9f:eb:56:a8")]
-    mac: String,
+    pub mac: String,
 }
 
-#[derive(ToSchema, Serialize, Deserialize, Clone)]
+#[derive(ToSchema, Serialize, Deserialize, Clone, Debug)]
 struct Config {
     machines: HashMap<String, MachineConfig>,
 }
 
+impl Config {
+    // TODO: move this logic in a service or something
+    fn by_name(&self, name: &str) -> Option<MachineConfig> {
+        self.machines.get(name).cloned()
+    }
+}
 
 #[derive(OpenApi)]
 #[openapi(
     nest(
-        (path = "/machine", api = machine::MachineApi)
+        (path = "/machine", api = machine::Api)
     ),
     tags(
         (name = "wol", description = "Power on and off computers API")
@@ -54,20 +60,25 @@ struct Config {
 struct ApiDoc;
 
 mod machine {
-    use std::{convert::Infallible, sync::{Arc, Mutex}};
+    use anyhow::Context as _;
+    use core::convert::Infallible;
+    use core::str::FromStr;
+    use log::{error, info};
+    use std::sync::{Arc, Mutex};
+    use wol::MacAddr;
 
-    use serde::{Deserialize, Serialize};
-    use utoipa::{IntoParams, OpenApi, ToSchema};
-    use warp::reply::{self, Reply};
+    use utoipa::OpenApi;
+    use warp::{
+        http, reject::Rejection, reply::{self, Reply}, Filter
+    };
 
-    use crate::Config;
+    use crate::{with_store, Config};
 
     pub type Store = Arc<Mutex<Config>>;
 
     #[derive(OpenApi)]
-    // #[openapi(paths(machine_list, machine_shutdown))]
-    #[openapi(paths(list))]
-    pub struct MachineApi;
+    #[openapi(paths(list, wake, shutdown))]
+    pub struct Api;
 
     // #[derive(Serialize, Deserialize, ToSchema)]
     // #[serde(rename_all = "snake_case")]
@@ -90,12 +101,105 @@ mod machine {
             (status = 200, description = "List machines successfully", body = super::Config)
         )
     )]
-    pub async fn list(
-        store: Store,
-    ) -> Result<impl Reply, Infallible> {
+    pub fn list(store: Store) -> impl warp::Reply {
         let machines = store.lock().unwrap();
-        Ok(reply::json(&machines.clone()))
+        reply::json(&machines.clone())
+    }
+
+    #[utoipa::path(
+        post,
+        path = "/{name}/shutdown",
+        responses(
+            (status = 200, description = "Shutdown the machine successfully"),
+            (status = 404, description = "Machine does not exist")
+        ),
+        params(
+            ("name" = String, Path, description = "Name of the machine to shutdown")
+        ),
+    )]
+    pub fn shutdown(
+        store: Store,
+        name: &str,
+        _dry_run: bool,
+    ) -> Result<Box<dyn Reply>, Infallible> {
+        let Some(machine) = store.lock().unwrap().by_name(name) else {
+            return Ok(Box::new(reply::with_status("Machine does not exist", http::StatusCode::NOT_FOUND)));
+        };
+        error!("TODO: machine shutdown {:#?}", machine);
+        Ok(Box::new(reply::reply()))
+    }
+
+    #[utoipa::path(
+        post,
+        path = "/{name}/wake",
+        responses(
+            (status = 200, description = "Woke the machine successfully"),
+            (status = 404, description = "Machine does not exist")
+        ),
+        params(
+            ("name" = String, Path, description = "Name of the machine to wake")
+        ),
+    )]
+    pub fn wake(store: Store, name: &str, dry_run: bool) -> Result<Box<dyn Reply>, Infallible> {
+        // TODO: change machine state
+        let Some(machine) = store.lock().unwrap().by_name(name) else {
+            return Ok(Box::new(reply::with_status("Machine does not exist", http::StatusCode::NOT_FOUND)));
+        };
+        let send_wol = match dry_run {
+            true => send_wol_dry_run,
+            false => send_wol,
+        };
+        match send_wol(&machine.mac) {
+            Ok(()) => Ok(Box::new(reply::reply())),
+            Err(e) => Ok(Box::new(e.to_string())),
         }
+    }
+
+    pub fn handlers(
+        config: &Config,
+        dry_run: bool,
+    ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+        let store = Store::new(Mutex::new(config.clone()));
+
+        let list = warp::path!("list")
+            .and(warp::get())
+            .and(with_store(store.clone()))
+            .map(list);
+
+        let wake = {
+            let store = store.clone();
+            warp::path!(String/"wake")
+                .map(move |mac_addr: String| wake(store.clone(), &mac_addr, dry_run))
+        };
+        let shutdown = {
+            let store = store.clone();
+            warp::path!(String/"shutdown" )
+                .map(move |mac_addr: String| shutdown(store.clone(), &mac_addr, dry_run))
+        };
+
+        list.or(wake).or(shutdown)
+    }
+    fn send_wol(mac_addr: &str) -> anyhow::Result<()> {
+        use wol::send_wol;
+        let mac_addr =
+            MacAddr::from_str(mac_addr).map_err(|err| anyhow::Error::msg(err.to_string()))?;
+        info!(
+            "Sending wake on lan to {}",
+            mac_addr.to_string().to_uppercase()
+        );
+        send_wol(mac_addr, None, None).context("Could not send wold")?;
+        Ok(())
+    }
+
+    fn send_wol_dry_run(mac_addr: &str) -> anyhow::Result<()> {
+        let mac_addr =
+            MacAddr::from_str(mac_addr).map_err(|err| anyhow::Error::msg(err.to_string()))?;
+        info!(
+            "Sending wake on lan to {}",
+            mac_addr.to_string().to_uppercase()
+        );
+        Ok(())
+    }
 }
 
 #[tokio::main]
@@ -109,54 +213,20 @@ async fn main() -> anyhow::Result<()> {
         .extract()
         .context("Failed to parse config file")?;
 
-    let store = Store::new(Mutex::new(config.clone()));
+    debug!("{config:?}");
 
-    let api_doc = warp::path!("api-doc.json").map(|| warp::reply::json(&ApiDoc::openapi()));
+    let api_doc = warp::path!("api-doc.json").map(|| reply::json(&ApiDoc::openapi()));
     let rapidoc_handler = warp::path("rapidoc")
         .and(warp::get())
-        .map(|| warp::reply::html(RapiDoc::new("/api-doc.json").to_html()));
+        .map(|| reply::html(RapiDoc::new("/api-doc.json").to_html()));
 
+    let machine_api = warp::path("machine").and(machine::handlers(&config, args.dry_run));
 
-    let machine_list =
-        warp::path!("machine"/"list").and(warp::get()).and(with_store(store.clone())).and_then(machine::list);
-
-    let machine_wake = warp::path!("wake" / String).map(move |mac_addr: String| {
-        let send_wol = match args.dry_run {
-            true => send_wol_dry_run,
-            false => send_wol,
-        };
-        match send_wol(mac_addr.as_str()) {
-            Ok(()) => "Ok".to_owned(),
-            Err(e) => e.to_string(),
-        }
-    });
-
-    let routes = warp::get().and(machine_wake.or(machine_list).or(api_doc).or(rapidoc_handler));
-    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+    warp::serve(api_doc.or(rapidoc_handler).or(machine_api))
+        .run(([0, 0, 0, 0], 3030))
+        .await;
     Ok(())
 }
- fn with_store(store: Store) -> impl Filter<Extract = (Store,), Error = Infallible> + Clone {
-        warp::any().map(move || store.clone())
-    }
-
-fn send_wol(mac_addr: &str) -> anyhow::Result<()> {
-    use wol::send_wol;
-    let mac_addr =
-        MacAddr::from_str(mac_addr).map_err(|err| anyhow::Error::msg(err.to_string()))?;
-    info!(
-        "Sending wake on lan to {}",
-        mac_addr.to_string().to_uppercase()
-    );
-    send_wol(mac_addr, None, None).context("Could not send wold")?;
-    Ok(())
-}
-
-fn send_wol_dry_run(mac_addr: &str) -> anyhow::Result<()> {
-    let mac_addr =
-        MacAddr::from_str(mac_addr).map_err(|err| anyhow::Error::msg(err.to_string()))?;
-    info!(
-        "Sending wake on lan to {}",
-        mac_addr.to_string().to_uppercase()
-    );
-    Ok(())
+fn with_store(store: Store) -> impl Filter<Extract = (Store,), Error = Infallible> + Clone {
+    warp::any().map(move || store.clone())
 }
