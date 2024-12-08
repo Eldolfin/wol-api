4ef0f26fef4d32706e800917e9fa510e289c8518
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-12-09 00:10:49 +0100

    feat(front): init vue project

---
+++ wol-panel/devenv.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  packages = [pkgs.yarn];
+
+  languages.typescript.enable = true;
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
