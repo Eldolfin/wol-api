2a6ffec94211086dc9feb8a10863a039acd9a339
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2024-12-21 18:22:48 +0100

    feat(api): wip config reloading

--- wol-api/tests/test_config.rs
+++ wol-api/tests/test_config.rs
@@ -1,11 +1,12 @@
-use std::{any::Any, collections::HashMap, fs::File, io};
+use std::{collections::HashMap, fs::File, io::Write, time::Duration};
 
 use anyhow::{Context, Result};
 use tempfile::TempDir;
+use tokio::time;
 use wol_relay_server::config::{self, Config, MachineCfg};
 
 #[tokio::test]
-async fn test_config_reload() -> Result<()> {
+async fn config_reload() -> Result<()> {
     const AUTO_RELOAD: bool = true;
 
     let dir = TempDir::new()?;
@@ -39,12 +40,76 @@ async fn test_config_reload() -> Result<()> {
 
     in_memory_config.machines.get_mut("machine1").unwrap().ip = "192.168.1.1".into();
 
+    let config_file = File::create(&config_filename).context("Could not re open config file")?;
+
     serde_yaml::to_writer(&config_file, &in_memory_config)
         .with_context(|| format!("Failed to write to {}", config_filename.display()))?;
+
+    // lets the config be reloaded
+    time::sleep(Duration::from_millis(10)).await;
+
     assert_eq!(
         in_file_config.lock().unwrap().clone(),
         in_memory_config,
         "Config differs after being changed"
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn config_reload_error() -> Result<()> {
+    const AUTO_RELOAD: bool = true;
+
+    let dir = TempDir::new()?;
+
+    let config_filename = dir.path().join("wol-config.yml");
+
+    let in_memory_config = Config {
+        machines: HashMap::from([(
+            "machine1".into(),
+            MachineCfg {
+                ip: "192.168.1.167".into(),
+                mac: "f4:93:9f:eb:56:a8".into(),
+                ssh_port: 22,
+                tasks: vec![],
+            },
+        )]),
+    };
+
+    let config_file = File::create_new(&config_filename).context("Could not create config file")?;
+
+    serde_yaml::to_writer(&config_file, &in_memory_config)
+        .with_context(|| format!("Failed to write to {}", config_filename.display()))?;
+
+    let in_file_config = config::open(&config_filename, AUTO_RELOAD)?;
+
+    assert_eq!(
+        in_file_config.lock().unwrap().clone(),
+        in_memory_config,
+        "Config differs before being changed"
+    );
+
+    let mut in_memory_config_modified = in_memory_config.clone();
+    in_memory_config_modified
+        .machines
+        .get_mut("machine1")
+        .unwrap()
+        .ip = "192.168.1.1".into();
+
+    let config_file = File::create(&config_filename).context("Could not re open config file")?;
+
+    serde_yaml::to_writer(&config_file, &in_memory_config_modified)
+        .with_context(|| format!("Failed to write to {}", config_filename.display()))?;
+    write!(&config_file, "      - random_field: false").unwrap();
+
+    // lets the config be reloaded
+    time::sleep(Duration::from_millis(10)).await;
+
+    assert_eq!(
+        in_file_config.lock().unwrap().clone(),
+        in_memory_config,
+        "Config should not be changed if there was an error loading it"
     );
 
     Ok(())
