1947dc3c6fe9364eaa249a87b5afc36d09190498
Oscar Le Dauphin <oscar.ledauphin@qarnot-computing.com>
2024-12-23 16:45:58 +0100

    feat(front): copiable buttons for ip and ssh connect command

---
+++ wol-panel/src/components/CopiableButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const value = defineModel<string>("value", {
+  required: true,
+});
+
+const { copy, copied, isSupported } = useClipboard({ source: value })
+
+function handleClick() {
+  copy()
+}
+
+</script>
+<template>
+  <n-button
+     @click="handleClick"
+     :disabled="!isSupported"
+  >
+    {{ copied ? 'Copied!' : value }}
+  </n-button>
+</template>
