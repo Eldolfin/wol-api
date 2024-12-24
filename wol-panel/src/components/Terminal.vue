746b91f769e0a7762fb2800145ad23a5168b8b10
Oscar Le Dauphin <oscar.ledauphin@qarnot-computing.com>
2024-12-24 17:47:28 +0100

    fix(front): yarn fmt

--- wol-panel/src/components/Terminal.vue
+++ wol-panel/src/components/Terminal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Terminal } from '@xterm/xterm';
-import {FitAddon} from "@xterm/addon-fit";
+import { FitAddon } from "@xterm/addon-fit";
 import '@xterm/xterm/css/xterm.css';
 import { AttachAddon } from '@xterm/addon-attach';
 import { baseUrl, terminal_pane_provide } from '../provides';
@@ -25,22 +25,22 @@ watchEffect(() => {
   }
 })
 
-onMounted(()=> {
+onMounted(() => {
   term.open(terminalElt.value!);
 
   watchDebounced([width, height], () => {
-  console.log("terminal size changed, calling fit on it")
+    console.log("terminal size changed, calling fit on it")
     fitAddon.fit();
     term.refresh(0, term.rows - 1);
-  }, {immediate: true, debounce: 100})
+  }, { immediate: true, debounce: 100 })
 })
 
 </script>
 <template>
-  <div ref="terminal-parent" :style="{display:'flex', flexFlow: 'column', height:'100%'}">
+  <div ref="terminal-parent" :style="{ display: 'flex', flexFlow: 'column', height: '100%' }">
     <!-- <div> -->
-      <!-- Currently connected to '{{terminalState.currentConnectedMachineName}}' -->
+    <!-- Currently connected to '{{terminalState.currentConnectedMachineName}}' -->
     <!-- </div> -->
-    <div ref="terminal" :style="{flex: '1 1 auto'}"></div>
+    <div ref="terminal" :style="{ flex: '1 1 auto' }"></div>
   </div>
 </template>
