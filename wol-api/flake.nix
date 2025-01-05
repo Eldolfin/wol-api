e7c4fc4767d746cc37fb5f91373cad9c75ba0179
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-01-05 17:54:43 +0100

    fix(backend): nixos service

--- wol-api/flake.nix
+++ wol-api/flake.nix
@@ -3,20 +3,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
     crane.url = "github:ipetkov/crane";
     rust-overlay.url = "github:oxalica/rust-overlay";
 
     # Dev tools
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    omnix.url = "github:juspay/omnix";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = import inputs.systems;
-
+      systems = ["x86_64-linux"];
       # See ./nix/modules/*.nix for the modules that are imported here.
       imports = with builtins;
         map
--- wol-api/nix/modules/nixos-service.nix
+++ wol-api/nix/modules/nixos-service.nix
@@ -1,50 +1,44 @@
-{inputs, ...}: {
-  perSystem = {
+{self, ...}: let
+  nixosModule = {
     config,
-    self',
-    pkgs,
     lib,
+    pkgs,
     ...
-  }: {
-    nixosModule = {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-      with lib; let
-        cfg = config.eldolfin.services.wol-agent;
-      in {
-        options.eldolfin.services.wol-agent = {
-          enable = mkEnableOption "Enables the wol-agent service";
+  }:
+    with lib; let
+      cfg = config.eldolfin.services.wol-agent;
+    in {
+      options.eldolfin.services.wol-agent = {
+        enable = mkEnableOption "Enables the wol-agent service";
 
-          domain = mkOption {
-            type = types.str;
-            default = "";
-            example = "192.168.1.1:3001";
-            description = "The domain name for the wol-backend";
-          };
+        domain = mkOption {
+          type = types.str;
+          default = "";
+          example = "192.168.1.1:3001";
+          description = "The domain name for the wol-backend";
         };
+      };
 
-        config = mkIf cfg.enable {
-          systemd.services."eldolfin.wol-agent" = {
-            wantedBy = ["multi-user.target"];
+      config = mkIf cfg.enable {
+        systemd.services."eldolfin.wol-agent" = {
+          wantedBy = ["multi-user.target"];
 
-            serviceConfig = let
-              pkg = self.packages.${pkgs.system}.default;
-            in {
-              Restart = "on-failure";
-              ExecStart = "${pkg}/bin/agent ${cfg.domain}";
-              DynamicUser = "yes";
-              RuntimeDirectory = "eldolfin.wol-agent";
-              RuntimeDirectoryMode = "0755";
-              StateDirectory = "eldolfin.wol-agent";
-              StateDirectoryMode = "0700";
-              CacheDirectory = "eldolfin.wol-agent";
-              CacheDirectoryMode = "0750";
-            };
+          serviceConfig = let
+            pkg = self.packages.${pkgs.system}.default;
+          in {
+            Restart = "on-failure";
+            ExecStart = "${pkg}/bin/agent ${cfg.domain}";
+            DynamicUser = "yes";
+            RuntimeDirectory = "eldolfin.wol-agent";
+            RuntimeDirectoryMode = "0755";
+            StateDirectory = "eldolfin.wol-agent";
+            StateDirectoryMode = "0700";
+            CacheDirectory = "eldolfin.wol-agent";
+            CacheDirectoryMode = "0750";
           };
         };
       };
-  };
+    };
+in {
+  flake.nixosModules.default = nixosModule;
 }
